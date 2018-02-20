e73a73d1c24bb1be14d3fce88891fc3f6f881df5
Mike Semko <msemko@gmail.com>
2018-02-20 03:12:37 -0500

    Ship faces the mouse cursor and strafes, see yodiz notes, @T21

--- scripts/airship.gd
+++ scripts/airship.gd
@@ -1,28 +1,25 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 export var SPEED = 300
-var velocity = Vector2()
-
-func _ready():
-	pass
-
-func _input(event):
-	if event.is_action_pressed("game_up"):
-		velocity.y -= SPEED
-	if event.is_action_pressed("game_down"):
-		velocity.y += SPEED
-	if event.is_action_pressed("game_left"):
-		velocity.x -= SPEED
-	if event.is_action_pressed("game_right"):
-		velocity.x += SPEED
-	if event.is_action_released("game_up"):
-		velocity.y += SPEED
-	if event.is_action_released("game_down"):
-		velocity.y -= SPEED
-	if event.is_action_released("game_left"):
-		velocity.x += SPEED
-	if event.is_action_released("game_right"):
-		velocity.x -= SPEED
+export var SIDE_SPEED = 100
+	
+func _physics_process(delta):
+	
+	var velocity = Vector2()
+	if Input.is_action_pressed("game_up"):
+		velocity.x += 1
+	if Input.is_action_pressed("game_down"):
+		velocity.x -= 1
+	if Input.is_action_pressed("game_left"):
+		velocity.y -= 1
+	if Input.is_action_pressed("game_right"):
+		velocity.y += 1
 		
-	linear_velocity = velocity.normalized() * SPEED
-	pass
+	if get_viewport().get_mouse_position().distance_to(position) > 50:
+		rotation = get_viewport().get_mouse_position().angle_to_point(position)
+		
+	velocity = velocity.normalized().rotated(rotation)
+	
+
+		
+	move_and_collide(velocity * SPEED * delta)
